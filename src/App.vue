c49cbb9630e46151a83586dcfc42a22b841b6aa4
Bévan <gregory.bevan@cbp-group.com>
2017-02-21 17:20:52 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Piece</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/scss/style.scss';
+</style>
