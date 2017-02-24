ab980d5e319a2257bcde9f8df9816ff7039a06af
Bévan <gregory.bevan@cbp-group.com>
2017-02-24 11:34:46 +0100

    Handle file upload

--- src/form/FormComponent.vue
+++ src/form/FormComponent.vue
@@ -1,11 +1,11 @@
 <template lang="html">
   <div class="container">
     <div class="row col-xs-12">
-      <form ref="main-form" id="main-form">
+      <form ref="mainForm">
         <div class="form-group col-xs-6">
           <label for="societe">Société</label>
-          <select id="societe" name="societe" class="form-control">
-            <option disabled selected value> -- Sélectionner -- </option>
+          <select id="societe" v-model="societe" class="form-control">
+            <option disabled value=""> -- Sélectionner -- </option>
             <option value="4" title="4">CBP France</option>
             <option value="11" title="11">CBP Italia</option>
             <option value="12" title="12">CBP Protection de Pagos</option>
@@ -14,8 +14,8 @@
         </div>
         <div class="form-group col-xs-6">
           <label for="domaine">Domaine</label>
-          <select id="domaine" name="domaine" class="form-control">
-            <option disabled selected value> -- Sélectionner -- </option>
+          <select id="domaine" v-model="domaine" class="form-control">
+            <option disabled value=""> -- Sélectionner -- </option>
             <option value="1" title="1">Adhésion</option>
             <option value="2" title="2">Sinistre</option>
             <option value="3" title="3">SR</option>
@@ -28,9 +28,6 @@
           <pieces-jointes></pieces-jointes>
         </div>
       </div>
-      <input type="hidden" name="type-reception" value="7">
-      <input type="hidden" name="idCheminRelatif" value="147">
-      <input type="hidden" name="etat" value="12">
       <div class="form-group col-xs-12">
         <p class=" text-center">
           <input type="submit" class="btn btn-primary" value="Envoyer" @click="submitForm">
@@ -47,37 +44,45 @@ import {EventBus} from '../bus.js'
 
 export default {
   name: 'form-component',
-  data() {
-	  return {
-		  piecePrincipaleFile: {}
-	  }
-  },
+  data: () => ({
+    societe: "",
+    domaine: "",
+    piecePrincipaleFile: {}
+  }),
   components: {
     PiecePrincipale,
     PiecesJointes
   },
-  created() {
-	  EventBus.$on('piecePrincipaleUpdated', function(file) {
-		 console.log(file);
-	})
+  mounted() {
+    EventBus.$on('piecePrincipaleUpdated', file => {
+      console.log("File to uplaod :");
+      console.log(file);
+      this.piecePrincipaleFile=file;
+    });
   },
   methods: {
-    'submitForm': function() {
-    	var formData = new FormData();
-    	formData.append('file', 'bar');
-    	this.$http.post('api/', formData).then((response) => {
-    		console.log('trop cool');
-    	}, (response) => {
-    		console.log('Putain ça marche pas');
-    	})
-    	this.$http.get('api/').then((response) => {
-            console.log('test')
-        }, (response) => {
-            // error callback
-        });
+    'submitForm': function(event) {
+      event.preventDefault();
+      console.log(this.piecePrincipaleFile);
+      var formData = this.getFormData();
+      this.$http.post('api/', formData).then((response) => {
+        console.log('trop cool');
+      }, (response) => {
+        console.log('Putain ça marche pas');
+      })
     },
     'piecePrincipaleUpdate': function(file) {
-    	console.log("Update piece principale");
+      console.log("Update piece principale");
+    },
+    'getFormData': function() {
+      var formData = new FormData();
+      formData.append('societe', this.societe);
+      formData.append('domaine', this.domaine);
+      formData.append('type-reception', 7);
+      formData.append('idCheminRelatif', 147);
+      formData.append('etat', 12);
+      formData.append('file', this.piecePrincipaleFile, this.piecePrincipaleFile.name);
+      return formData;
     }
   }
 }
